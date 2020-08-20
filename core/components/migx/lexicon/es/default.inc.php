6bb364205a1c96fb517b3f1bfd88ed473205898b
Mark Hamstra <hello@markhamstra.com>
2020-08-20 15:25:22 +0200

    traducido

---
+++ core/components/migx/lexicon/es/default.inc.php
@@ -0,0 +1,80 @@
+<?php
+/**
+ * migx
+ *
+ * @package migx
+ * @language en
+ */
+
+//$_lang['mig_'] = '';
+
+$_lang['migx'] = 'MIGX';
+$_lang['migx.management'] = 'Gestión de MIGX';
+$_lang['migx.management_desc'] = 'Administra tus configuraciones de MIGX aquí. Puedes editarlas haciendo clic derecho en la fila respectiva.';
+
+$_lang['migx.noitems'] = 'No se encontraron elementos';
+$_lang['migx.add'] = 'Añadir elemento';
+$_lang['migx.remove_confirm'] = '¿Borrar elemento?';
+$_lang['migx.edit'] = 'Editar';
+$_lang['migx.edit_raw'] = 'Edit en crudo';
+$_lang['migx.edit_flat'] = 'Pestañas planas';
+$_lang['migx.export_import'] = 'Exportar/Importar';
+$_lang['migx.remove'] = 'Eliminar';
+$_lang['migx.duplicate'] = 'Duplicar';
+$_lang['migx.preview'] = 'Vista previa';
+$_lang['migx.save_resource'] = '¡Antes de agregar nuevos elementos, debe guardar este recurso!';
+$_lang['migx.save_object'] = '¡Antes de agregar nuevos elementos, debe guardar este objeto!';
+
+$_lang['migx.show_trash'] = 'Vista de papelera';
+$_lang['migx.show_normal'] = 'Vista normal';
+$_lang['migx.loadgrid'] = 'Cargar rejilla';
+
+$_lang['migx.create'] = 'Crear';
+$_lang['migx.publish'] = 'Publicar';
+$_lang['migx.unpublish'] = 'Despublicar';
+$_lang['migx.delete'] = 'Borrar';
+$_lang['migx.actions'] = 'Acciones';
+$_lang['migx.bulk_actions'] = 'Acciones en masa';
+$_lang['migx.publish_selected'] = 'Publicar seleccionados';
+$_lang['migx.unpublish_selected'] = 'Despublicar seleccionados';
+$_lang['migx.delete_selected'] = 'Borrar seleccionados';
+$_lang['migx.remove_selected'] = 'Eliminar seleccionados';
+$_lang['migx.export_current_view'] = 'Exportat vista actual';
+$_lang['migx.recall'] = 'Llamar';
+$_lang['migx.remove'] = 'Eliminar';
+$_lang['migx.new'] = 'nuevo';
+$_lang['migx.filters'] = 'Flitros';
+$_lang['migx.move_to_top'] = 'Mover al principio';
+$_lang['migx.move_to_bottom'] = 'Mover al final';
+
+$_lang['migx.add_replacement'] = 'Reemplazo de "Añadir elemento"';
+$_lang['migx.activate'] = 'Activar';
+$_lang['migx.deactivate'] = 'Desactivar';
+
+$_lang['migx.import_from_package'] = 'Importar desde paquete';
+$_lang['migx.export_to_package'] = 'Exportar a paquete';
+
+$_lang['migx.month'] = 'Mes';
+$_lang['migx.year'] = 'Año';
+
+$_lang['migx.upload_images'] = 'Subir archivos';
+$_lang['migx.load_from_source'] = 'Cargar desde fuente de medios';
+$_lang['migx.max_records_alert'] = 'Se alcanzó la cantidad máxima de registros';
+
+$_lang['migx.selector_options'] = 'Opciones';
+
+$_lang['migx.emptytrash_confirm'] = '¿Seguro que quieres vaciar la papelera?';
+$_lang['migx.emptytrash'] = 'Vaciar papelera';
+$_lang['migx.search'] = 'Buscar...';
+$_lang['migx.reset_all'] = 'Resetear todo';
+$_lang['migx.import_csv'] = 'Importar CSV';
+$_lang['migx.add_field'] = 'Añadir campo';
+$_lang['migx.add_formtab'] = 'Añadir pestaña al formulario';
+$_lang['migx.add_formlayout'] = 'Diseño';
+$_lang['migx.add_layoutcolumn'] = 'Columna de diseño';
+
+$_lang['migx.add_item_before'] = 'Añadir elemento antes';
+$_lang['migx.add_item_after'] = 'Añadir elemento después';
+
+$_lang['migx.select_dbfields'] = 'Seleccionar campos de la base de datos';
+$_lang['migx.select_dbfields_desc'] = 'Antes de que puedas seleccionar db-fields, tienes que poner algo en packageName, classname y joins-configs y guardar la configuración.';
---
+++ core/components/migx/lexicon/es/tvprops.inc.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * migx
+ *
+ * @package migx
+ * @language en
+ */
+
+
+$_lang['mig.tabs'] = 'Pestañas de formulario';
+$_lang['mig.columns'] = 'Columnas de la rejilla';
+$_lang['mig.btntext'] = 'Reemplazo de "Añadir elemento"';
+$_lang['mig.previewurl'] = 'Url de vista previa';
+$_lang['mig.jsonvarkey'] = 'Vista previa JsonVarKey';
+$_lang['mig.configs'] = 'Configuraciones';
+$_lang['mig.autoResourceFolders'] = 'Carpetas de recursos automáticos';
